071ad42ca9731a67a5057ea647c7794cf838ca71
rosietie <Rosietie2@gmail.com>
2023-07-19 15:31:30 -0700

    Putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - wrinting a programm that print _putchar
+*
+* Return:always 0
+*/
+int main(void)
+{
+__putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
