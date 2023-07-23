efcf89b8350d0bf7f98001a5586ca8852f46fede
rosietie <Rosietie2@gmail.com>
2023-07-23 15:00:27 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 int i;
@@ -15,6 +13,5 @@ i = 0;
 /*putchar(i);*/
 /*}*/
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
---
+++ 0x03-debugging/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - multipulation function
+* @a: arguments
+* @b: arguments
+* Return: Product of operation
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
