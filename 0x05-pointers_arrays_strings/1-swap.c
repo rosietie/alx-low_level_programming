22c017c9219133af6ab177a17789fe55882631ad
rosietie <Rosietie2@gmail.com>
2023-07-25 14:54:04 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
-#include "main.h"i
+#include "main.h"
 /**
-* swap_int - swaping the values of two integers
+* swap_int-swaps the value of the two integers
 * @a: integer to swap
 * @b: integer to swap
 */
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b);
 {
 	int m;
-
-	m = *a;
+	
+m = *a;
 	*a = *b;
 	*b = m;
 }
